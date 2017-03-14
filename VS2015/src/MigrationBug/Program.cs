682ce986397d5b67331ffb884ea626b1302f0276
Pavel Werl <pavel@werl.cz>
2017-03-14 14:20:41 +0100

    Imported bugs

---
+++ VS2015/src/MigrationBug/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MigrationBug
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Press enter to begin");
+            Console.ReadLine();
+
+            using (var client = new HttpClient())
+                Console.WriteLine(client.GetStringAsync("http://www.contoso.com").Result);
+
+            Console.WriteLine("Finished! Press enter to exit.");
+            Console.ReadLine();
+        }
+    }
+}
